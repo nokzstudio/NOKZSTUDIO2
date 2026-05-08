029b8eaa5f5ad4da784afa9008a0f875a401c289
nokzstudio <nokzdesign@gmail.com>
2026-05-08 13:28:40 +0700

    aaw

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -75,7 +75,7 @@ import {
   Legend 
 } from 'recharts';
 
-import { } from '../lib/constants';
+import from '../lib/constants';
 
 import { Skeleton, CardSkeleton, AdminRowSkeleton } from './Skeleton';
 
@@ -830,7 +830,6 @@ export default function Admin({ onBack }: { onBack: () => void }) {
           </button>
         </motion.div>
       </div>
-    );
   }
 
   return (
